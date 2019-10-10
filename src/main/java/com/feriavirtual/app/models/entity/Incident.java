1f65419a85424036dded78df711ce170ede44620
Ro <rodolfo.quezada.s@gmail.com>
2019-10-10 18:01:52 -0300

    CRUD de inicidencias

--- src/main/java/com/feriavirtual/app/models/entity/Incident.java
+++ src/main/java/com/feriavirtual/app/models/entity/Incident.java
@@ -1,4 +1,29 @@
 package com.feriavirtual.app.models.entity;
 
-public class Incident {
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Data
+@Table(name = "incidents")
+public class Incident implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.SEQUENCE, generator = "SEQ_INCIDENT")
+    @SequenceGenerator(name = "SEQ_INCIDENT",allocationSize = 1,sequenceName = "SEQ_INCIDENT")
+    private Long id;
+
+    private String message;
+    private Boolean status;
+    private String emisor;
+    private String receptor;
+
+    public Incident(){
+        this.status = true;
+    }
+
+
+
 }
