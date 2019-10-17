530769cc19e4d71ea28e64ad8490410906770e3f
Ro <rodolfo.quezada.s@gmail.com>
2019-10-16 22:57:49 -0300

    ultimos cambios

---
+++ src/main/java/com/feriavirtual/app/models/entity/Authority.java
@@ -0,0 +1,21 @@
+package com.feriavirtual.app.models.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+
+@Entity
+@Data
+@Table(name = "authorities")
+public class Authority implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.SEQUENCE, generator = "SEQ_AUTHORITY")
+    @SequenceGenerator(name = "SEQ_AUTHORITY",allocationSize = 1,sequenceName = "SEQ_AUTHORITY")
+    private Long id;
+    private String authority;
+
+
+}
