5c0c4345af92404b9446fb3c1d92bc2a12c35338
Ro <rodolfo.quezada.s@gmail.com>
2019-10-19 18:14:35 -0300

    relación de categoria con producto.

--- src/main/java/com/feriavirtual/app/models/entity/Category.java
+++ src/main/java/com/feriavirtual/app/models/entity/Category.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.List;
 
 @Entity
 @Data
@@ -14,13 +15,16 @@ import java.io.Serializable;
 @Table(name = "categories")
 public class Category implements Serializable {
 
-
     @Id
     @GeneratedValue(strategy= GenerationType.SEQUENCE, generator = "SEQ_CATEGORY")
     @SequenceGenerator(name = "SEQ_CATEGORY",allocationSize = 1,sequenceName = "SEQ_CATEGORY")
     private Long id;
     private String name;
     private String description;
+    private String image;
+
+    @OneToMany(mappedBy = "category", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<Product> products;
 
     private static final long serialVersionUID = 2625797175843733106L;
 
