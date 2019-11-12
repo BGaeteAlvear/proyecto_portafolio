372e41f58c53781a626be8d419bf9331347b049e
LauraDuoc <laur.gomez@alumnos.duoc.cl>
2019-11-11 22:59:43 -0300

    Validación de categorias y prod

--- src/main/java/com/feriavirtual/app/models/entity/Category.java
+++ src/main/java/com/feriavirtual/app/models/entity/Category.java
@@ -38,8 +38,29 @@ public class Category implements Serializable {
         return image;
     }
 
+    public Long getId() {
+        return Id;
+    }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
 }
 
 
