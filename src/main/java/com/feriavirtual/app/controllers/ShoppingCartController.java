13c9ea0820d5b6d89d91b16d69a94867c25c077a
Brian Gaete Alvear <b.gaete.digilab@gmail.com>
2019-12-09 17:08:19 -0300

    ajustes

--- src/main/java/com/feriavirtual/app/controllers/ShoppingCartController.java
+++ src/main/java/com/feriavirtual/app/controllers/ShoppingCartController.java
@@ -1,21 +1,19 @@
 package com.feriavirtual.app.controllers;
 
-import com.feriavirtual.app.models.entity.Category;
-import com.feriavirtual.app.models.entity.Person;
-import com.feriavirtual.app.models.entity.Product;
-import com.feriavirtual.app.models.entity.ProductAvailable;
+import com.feriavirtual.app.models.entity.*;
 import com.feriavirtual.app.models.service.ICategoryService;
 import com.feriavirtual.app.models.service.IProductAvailableService;
 import com.feriavirtual.app.models.service.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -32,8 +30,8 @@ public class ShoppingCartController {
 
     @GetMapping("/index")
     public String index (Model model, HttpSession session){
-        Person productor =  (Person) session.getAttribute("userSession");
         List<Category> listCategory = categoryService.getAll();
+        Person productor =  (Person) session.getAttribute("userSession");
         System.out.println("cantidad de productos : "+productAvailableService.findByPerson(productor).size());
         /* DATOS TEMPLATE */
         model.addAttribute("title_header", "TIENDA DE PRODUCTOS");
@@ -64,4 +62,15 @@ public class ShoppingCartController {
         model.addAttribute("product", product);
         return "/shopping-cart/detail";
     }
+
+    @PostMapping("/product/request/order-process")
+    public String orderValidation(@PathVariable(value = "id")Long id, BindingResult result, Model model){
+        Product product = productService.findById(id);
+        model.addAttribute("title_header", "TIENDA DE PRODUCTOS");
+        model.addAttribute("title_page", "PLATAFORMA MAIPO GRANDE | "+product.getName().toUpperCase());
+        model.addAttribute("subtitle_header","PRODUCTOS  / "+product.category.getName().toUpperCase()+" / "+product.getName().toUpperCase());
+        model.addAttribute("product", product);
+        return "/shopping-cart/detail";
+    }
+
 }
--- src/main/java/com/feriavirtual/app/models/repository/IPersonRepository.java
+++ src/main/java/com/feriavirtual/app/models/repository/IPersonRepository.java
@@ -1,12 +1,14 @@
 package com.feriavirtual.app.models.repository;
 
+import com.feriavirtual.app.models.entity.Authority;
 import com.feriavirtual.app.models.entity.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface IPersonRepository extends JpaRepository<Person, Long > {
 
     Person findByUsername(String username);
-
 
 }
